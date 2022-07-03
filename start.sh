ae1eab93378c7044365714acb5007131059f1c2c
Kyle Armstrong <kylearmstrong96@outlook.com>
2022-07-03 15:18:51 -0400

    changes

--- start.sh
+++ start.sh
@@ -4,17 +4,16 @@
 # set -x
 
 # Setup error handling
-set -o pipefail  # trace ERR through pipes
-set -o errtrace  # trace ERR through 'time command' and other functions
-set -o nounset   # set -u : exit the script if you try to use an uninitialised variable
-set -o errexit   # set -e : exit the script if any statement returns a non-true return value
+set -o pipefail # trace ERR through pipes
+set -o errtrace # trace ERR through 'time command' and other functions
+set -o nounset  # set -u : exit the script if you try to use an uninitialised variable
+set -o errexit  # set -e : exit the script if any statement returns a non-true return value
 
 # Print the user we're currently running as
 echo "Running as user: $(whoami)"
 
 # Define the exit handler
-exit_handler()
-{
+exit_handler() {
 	echo ""
 	echo "Waiting for server to shutdown.."
 	echo ""
@@ -31,11 +30,10 @@ exit_handler()
 trap 'exit_handler' SIGHUP SIGINT SIGQUIT SIGTERM
 
 # Define the install/update function
-install_or_update()
-{
+install_or_update() {
 	# Install Starbound from install.txt
 	echo "Installing or updating Starbound.. (this might take a while, be patient)"
-  steamcmd +runscript /app/install.txt
+	steamcmd +runscript /app/install.txt
 
 	# Terminate if exit code wasn't zero
 	if [ $? -ne 0 ]; then
@@ -49,12 +47,12 @@ if [ ! -z "$STEAM_USERNAME" ] && [ ! -z "$STEAM_PASSWORD" ]; then
 	# Setup username/password for Steam
 	sed -i "s/login anonymous/login $STEAM_USERNAME $STEAM_PASSWORD/g" /app/install.txt
 
-  ## FIXME: If we do this, then the installation will prompt us again for the Auth Code, not sure why?!
-  # # Attempt to login
-  # steamcmd +login $STEAM_USERNAME $STEAM_PASSWORD +quit
+	## FIXME: If we do this, then the installation will prompt us again for the Auth Code, not sure why?!
+	# # Attempt to login
+	# steamcmd +login $STEAM_USERNAME $STEAM_PASSWORD +quit
 else
-  echo "Missing STEAM_USERNAME and/or STEAM_PASSWORD, unable to continue!"
-  exit 1
+	echo "Missing STEAM_USERNAME and/or STEAM_PASSWORD, unable to continue!"
+	exit 1
 fi
 
 # Create the necessary folder structure
@@ -66,14 +64,14 @@ fi
 # Check that Starbound exists in the first place
 if [ ! -f "/steamcmd/starbound/linux/starbound_server" ]; then
 	# Install or update Starbound
-  install_or_update
+	install_or_update
 else
 	# Check if we should skip updates
 	if [ ! -z "$SKIP_STEAMCMD" ]; then
-    echo "Skip steamcmd set, skipping update checks.."
+		echo "Skip steamcmd set, skipping update checks.."
 	else
 		# Install or update Starbound
-    install_or_update
+		install_or_update
 	fi
 fi
 
@@ -81,13 +79,13 @@ echo "Handling downloaded mods..."
 
 # Move downloaded workshop items to starbound mods folder
 if [ ! -f "/steamcmd/starbound/steamapps/workshop/content/211820" ]; then
-	# Set the working directory
-	cd /steamcmd/starbound/steamapps/workshop/content/211820 || exit 1
-	for i in $(find -name \*.pak); do # Will break on whitespace, shouldn't be an issue
-    	echo "$i"
+	cd /steamcmd/starbound/steamapps/workshop/content/211820 || exit 1 # Set the working directory
+	for i in $(find -name \*.pak); do
+		echo "$i"
 	done
 else
 	echo "Unable to move mods over to mods folder as no workshop folder exists, mods will not be loaded!"
+fi
 
 # Set the working directory
 cd /steamcmd/starbound/linux || exit 1
